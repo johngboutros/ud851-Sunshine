6ffc419ace6bedf006bd367456b665f3ffcaaebc
John Boutros <john.g.boutros@gmail.com>
2017-12-29 00:02:27 +0200

    JB - S10.03-Exercise-FirebaseJobDispatcher resolved!

--- S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
+++ S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
@@ -1,4 +1,4 @@
-/*
+package com.example.android.sunshine.sync;/*
  * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,13 +13,76 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// TODO (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
+// (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
 
-// TODO (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+import android.os.AsyncTask;
 
-//  TODO (4) Declare an ASyncTask field called mFetchWeatherTask
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
-//  TODO (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
-//              TODO (6) Once the weather data is sync'd, call jobFinished with the appropriate arguments
+// (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+public class SunshineFirebaseJobService extends JobService {
 
-//  TODO (7) Override onStopJob, cancel the ASyncTask if it's not null and return true
+    // (4) Declare an ASyncTask field called mFetchWeatherTask
+    AsyncTask mFetchWeatherTask;
+
+
+    // (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
+    /**
+     * The entry point to your Job. Implementations should offload work to another thread of execution
+     * as soon as possible because this runs on the main thread. If work was offloaded, call {@link
+     * JobService#jobFinished(JobParameters, boolean)} to notify the scheduling service that the work
+     * is completed.
+     * <p>
+     * <p>If a job with the same service and tag was rescheduled during execution {@link
+     * JobService#onStopJob(JobParameters)} will be called and the wakelock will be released. Please
+     * make sure that all reschedule requests happen at the end of the job.
+     *
+     * @param job
+     * @return {@code true} if there is more work remaining in the worker thread, {@code false} if the
+     * job was completed.
+     */
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+
+        mFetchWeatherTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                SunshineSyncTask.syncWeather(SunshineFirebaseJobService.this);
+                return null;
+            }
+
+            // (6) Once the weather data is sync'd, call jobFinished with the appropriate arguments
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(job, false);
+            }
+        };
+
+        mFetchWeatherTask.execute();
+
+        return true;
+    }
+
+
+    // (7) Override onStopJob, cancel the ASyncTask if it's not null and return true
+    /**
+     * Called when the scheduling engine has decided to interrupt the execution of a running job, most
+     * likely because the runtime constraints associated with the job are no longer satisfied. The job
+     * must stop execution.
+     *
+     * @param job
+     * @return true if the job should be retried
+     * @see JobInvocation.Builder#setRetryStrategy(RetryStrategy)
+     * @see RetryStrategy
+     */
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        if (mFetchWeatherTask != null) mFetchWeatherTask.cancel(true);
+        return true;
+    }
+
+
+
+
+}
